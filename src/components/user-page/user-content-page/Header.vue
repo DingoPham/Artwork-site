a36bc7a589b2eed844edba8ef38450f106d0384e
DingoPham <pencibambo@gmail.com>
2024-08-22 16:24:22 +0700

    add left sidebar

--- src/components/user-page/user-content-page/Header.vue
+++ src/components/user-page/user-content-page/Header.vue
@@ -1,20 +1,34 @@
 <template>
     <div>
-        <div class="navbar w-100p h-60px flex justify-center items-center">
-            <div class="f-30">
-                <router-link active-class="a-c"to = '/'> Artwork </router-link>
-            </div>
-            <LeftNavbar />
-            <router-view></router-view>
+        <div class="navbar w-100p h-60px flex items-center justify-center">
+                <div class="f-30">
+                    <router-link active-class="a-c"to = '/'> Artwork </router-link>
+                </div>
+                <router-view></router-view>
         </div>
+        <div class="po-rel">
+            <LeftSidebarContent :isActive="isSidebarActive" @toggle-sidebar = "toggleSidebar" />
+        </div>
+        <LeftSidebar :isActive="isSidebarActive" />
     </div>
 </template>
 
 <script>
-import LeftNavbar from './user-other-funtion/LeftNavbar.vue';
+import LeftSidebar from './user-other-funtion/left-sidebar/LeftSidebar.vue';
+import LeftSidebarContent from './user-other-funtion/left-sidebar/LeftSidebarContent.vue';
 
 export default{
     name: 'Header',
-    components:{LeftNavbar}
+    components:{LeftSidebar, LeftSidebarContent},
+    data(){
+        return{
+            isSidebarActive: false
+        };
+    },
+    methods:{
+        toggleSidebar(){
+            this.isSidebarActive = !this.isSidebarActive
+        }
+    }
 }
 </script>
--- src/components/user-page/user-content-page/user-other-funtion/LeftNavbar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="po-fixed"></div>
-</template>
-
-<script>
-    export default{
-        name: 'LeftNavbar'
-    }
-</script>
