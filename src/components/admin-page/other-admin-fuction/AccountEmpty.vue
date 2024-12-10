98278b2a99838a15bcb9c964bc1422197a004268
DingoPham <pencibambo@gmail.com>
2024-12-10 17:33:29 +0700

    add ssl 3

--- src/components/admin-page/other-admin-fuction/AccountEmpty.vue
+++ src/components/admin-page/other-admin-fuction/AccountEmpty.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="ui">
         <div class="ui-1">
-            <h1>You are not Sign In</h1>
+            <h1>You are not Signed In</h1>
             <h2>Please <router-link to="/login">Sign In</router-link> to know more about the feature.</h2>
             <h2>Or if you don't have an account, just <router-link to="/register">Sign Up.</router-link> </h2>
         </div>
