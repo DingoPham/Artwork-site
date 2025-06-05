6187d035ce394bae209201df22ff7723e7d2b168
DingoPham <pencibambo@gmail.com>
2025-06-05 21:38:45 +0700

    test icon drive gg

--- src/components/other-functions/button-container/DarkButton.vue
+++ src/components/other-functions/button-container/DarkButton.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
-        <img class="w-30 h-30" src="https://res.cloudinary.com/dtqyzzv0h/image/upload/v1718735896/sun_g7ttgc.svg" alt="" />
+        <img class="w-30 h-30" src="https://drive.google.com/file/d/1Hl6BJfefwg1FIGOPZcGwzx7ZacWXtKJz/view?usp=sharing
+        " alt="" />
     </div>
 </template>
--- src/components/other-functions/button-container/LightButton.vue
+++ src/components/other-functions/button-container/LightButton.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        <img class="w-30 h-30" src="https://res.cloudinary.com/dtqyzzv0h/image/upload/v1718735890/moon_oysvki.svg" alt="" />
+        <img class="w-30 h-30" src="https://drive.google.com/file/d/1P1er3LNMUvgKAg6hlhMaznt77CcuhVei/view?usp=sharing" alt="" />
     </div>
 </template>
