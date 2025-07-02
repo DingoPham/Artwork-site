61a6695a423a5d51d9e545452ef44667d835a006
DingoPham <pencibambo@gmail.com>
2025-07-02 09:09:13 +0700

    add scrollbar and edit the flood icon

--- src/components/user-page/user-sidebar/Sidebar.vue
+++ src/components/user-page/user-sidebar/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="sidebar po-fixed w-300px h-100p f-20" :class="{ active: isActive }">
+    <div class="sidebar po-fixed w-300px h-100p f-20 mostly-customized-scrollbar" :class="{ active: isActive }">
       <div class="m-t-l">
         <p class="m-b tx-w f-30-n">Home</p>
         <router-link to="/">Art Gallery</router-link>
@@ -25,3 +25,18 @@
     }
   }
 </script>
+
+<style scoped>
+.mostly-customized-scrollbar {
+  display: block;
+  overflow: auto;
+}
+.mostly-customized-scrollbar::-webkit-scrollbar {
+  width: 5px;
+  height: 8px;
+  background-color: #868686;
+}
+.mostly-customized-scrollbar::-webkit-scrollbar-thumb {
+  background: #282828;
+}
+</style>
